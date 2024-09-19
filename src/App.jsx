dcc35d3fe3b1dc79ec033fce0e39194a0724d7e7
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-09-19 11:29:49 -0600

    feat (App): implement weather API call
        - Implemented the call to the weather API in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
+import { useEffect } from 'react';
+
+import Card from './components/Card.jsx'
 
 function App() {
 
+  const URL = 'https://api.openweathermap.org/data/2.5/weather'
+  const API_KEY = 'a57c9e23bfa0b457b14e592bd72c87eb'
+
+  useEffect(() => {
+    async function fetchWeatherData() {
+      const res = await fetch(`${URL}?q=${'London'}&apiKey=${API_KEY}`)
+      const data = await res.json()
+      console.log(data.weather)
+    }
+    fetchWeatherData()
+  }, []);
+
   return (
-    <>
-      
-    </>
+    <div>
+      <Card></Card>
+    </div>
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <div className="container">
+                hi
+            </div>
+        </div>
+    )
+}
+
+export default Card
