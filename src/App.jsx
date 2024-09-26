6b0d0dc38c218b3a90db119a7e8457db67fe9f5c
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-09-26 12:48:52 -0600

    feat (scripts): add fetch-utils.js for API fetch functions and improve icon logic
        - Added fetch-utils.js file for functions related to fetching data from an API.
        - Modified icon logic to be more scalable and simpler.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useEffect } from 'react';
-import { fetchWeatherData } from './scripts/fetchWeatherData.js'
-import { getWeather } from './scripts/getWeather.js'
+import { fetchWeatherData } from './utils/fetch-utils.js'
 
 import Card from './components/Card.jsx'
 
@@ -16,43 +15,50 @@ function App() {
   const URL = 'https://api.openweathermap.org/data/2.5/weather'
   const API_KEY = 'a57c9e23bfa0b457b14e592bd72c87eb'
 
-  useEffect(() => {
+  const isArrayEmpty = (array) => {
 
-    const fetchWeather = async () => {
-      const weatherData = await getWeather(city, URL, API_KEY)
-      setWeatherInfo(weatherData)
+  }
+
+  const fetchWeather = async () => {
+    try {
+      const weatherData = await fetchWeatherData(city, URL, API_KEY)
+      if (weatherData && weatherData.cityName && weatherData.weather) {
+        setWeatherInfo(weatherData)
+      } else {
+        console.error('Weather data is incomplete:', weatherData)
+      }
+    } catch (error) {
+      console.error('Error fetching weather data:', error)
     }
+  }
+
+  useEffect(() => {
 
     fetchWeather()
 
-    if (weatherInfo.weather === 'clear sky') {
-      setIcon('./src/assets/weather-icons/sun.svg')
-    }
-    else if (weatherInfo.weather === 'few clouds') {
-      setIcon('./src/assets/weather-icons/cloud-sun.svg')
-    }
-    else if (weatherInfo.weather === 'scattered clouds') {
-      setIcon('./src/assets/weather-icons/cloud.svg')
-    }
-    else if (weatherInfo.weather === 'broken clouds') {
-      setIcon('./src/assets/weather-icons/multiple-clouds.svg')
-    }
-    else if (weatherInfo.weather === 'shower rain') {
-      setIcon('./src/assets/weather-icons/cloud-showers.svg')
-    }
-    else if (weatherInfo.weather === 'rain') {
-      setIcon('./src/assets/weather-icons/cloud-rain.svg')
-    }
-    else if (weatherInfo.weather === 'thunderstorm') {
-      setIcon('./src/assets/weather-icons/cloud-bolt.svg')
-    }
-    else if (weatherInfo.weather === 'snow') {
-      setIcon('./src/assets/weather-icons/snowflake.svg')
-    }
-    else if (weatherInfo.weather === 'mist') {
-      setIcon('./src/assets/weather-icons/mist.svg')
-    }
+    //useEffect will update the weatherInfo state when the city changes
   }, [city]);
+
+  useEffect(() => {
+    const weatherIcons = {
+      'clear sky': './src/assets/weather-icons/sun.svg',
+      'few clouds': './src/assets/weather-icons/cloud-sun.svg',
+      'scattered clouds': './src/assets/weather-icons/cloud.svg',
+      'broken clouds': './src/assets/weather-icons/multiple-clouds.svg',
+      'shower rain': './src/assets/weather-icons/cloud-showers.svg',
+      'rain': './src/assets/weather-icons/cloud-rain.svg',
+      'thunderstorm': './src/assets/weather-icons/cloud-bolt.svg',
+      'snow': './src/assets/weather-icons/snowflake.svg',
+      'mist': './src/assets/weather-icons/mist.svg',
+      'moderate rain': './src/assets/weather-icons/cloud-showers.svg'
+    };
+
+    const iconPath = weatherIcons[weatherInfo.weather];
+    if (iconPath) {
+      setIcon(iconPath);
+    }
+    // useEffect will update the icon state when the weatherInfo state changes
+  }, [weatherInfo])
 
 
   const changeCity = (name) => setCity(name)
